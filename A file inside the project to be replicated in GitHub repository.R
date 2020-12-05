e1f181abdf9ef7af1ee733941d0101775d665a6e
Juan L Aguirre <juanlauro@me.com>
2020-12-05 17:04:13 -0600

    think wont affect changes

---
+++ A file inside the project to be replicated in GitHub repository.R
@@ -0,0 +1,2 @@
+Hello here we go again, this is a file for SECOND project that is being replicated in a repository in GitHub in RStudio is a project and in GitHub is a repository
+hello frinds from Rstudio
